b15d8ccddd232bdbecd8ad158b779791f2200bd5
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2021-08-08 06:08:39 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('TG_Token', 'PUT_TELEGRAM_BOT_TOKEN_HERE');   // Obtain in @BotFather (e.g., 123:AAAJqs_w-4)
-define('lineChannelSecret', 'PUT_CHANNEL_SECRET_HERE'); // LINE Channel Secret (32 hex string)
-define('lineChannelAccessToken', 'PUT_CHANNEL_ACCESS_TOKEN_HERE');   // LINE Channel Access Token (base64 string)
+define('TG_Token', '1946274248:AAHHqm0_3mi4E5rzAzB1iIg40mC5ECIFzdc');   // Obtain in @BotFather (e.g., 123:AAAJqs_w-4)
+define('lineChannelSecret', '93880b0b6c783bd38205028fbd9dc623'); // LINE Channel Secret (32 hex string)
+define('lineChannelAccessToken', '1r0WXG6wHw+UYllpx+iWNP90L8mFCh4pjRLKSCXWXdYranwpOGrAW+8XY+xLRLdCzEMDByhwEHhGU215jePW1PJHuRlG20hWHV4QM5f5bku95OiZotp60dc8cgfc8dc8cgwmfx60dbc8cgwcgwc8dcgwc8dcgwpgrldcsxwxdyranwpgraw+8xy+xLRLdCzEMD');   // LINE Channel Access Token (base64 string)
 $CHAT = [
 	'C87630000000000000000000000000000' => -1001068773197,   // Forward Chat 1
 	'ERROR' => -1001068773197   // Error log, other chat
